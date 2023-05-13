f4679698e4b1f8d7023045e17fe839111a38f048
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2023-05-13 15:29:13 +0530

    testing

--- src/constants.py
+++ src/constants.py
@@ -3,8 +3,8 @@ import os
 ROOT_DIR = os.getcwd()
 MODEL_DIR_NAME = "serving_model"
 MODEL_NAME = "roberta-fine-tuned-2"
-MODEL_PATH = os.path.join(ROOT_DIR, MODEL_NAME)
+MODEL_PATH = os.path.join(ROOT_DIR, MODEL_DIR_NAME, MODEL_NAME)
 TOKENIZER_FILE_NAME = "tokenizer"
-TOKENIZER_PATH = os.path.join(ROOT_DIR, TOKENIZER_FILE_NAME)
+TOKENIZER_PATH = os.path.join(ROOT_DIR, MODEL_DIR_NAME, TOKENIZER_FILE_NAME)
 MAX_LEN =192
 BUFFER_SIZE=2048
